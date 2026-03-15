3a74bb2fb957b4b36c6a83eec53d06f468d6d375
Zakaria <elkorchizakariya@example.com>
2026-03-15 00:54:01 +0000

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path' 
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/portfolio-frontend/',  
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
